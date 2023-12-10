e29672ee62ec0aadd9b694bcf2f3239261591d6b
Preston-Wong <you@example.com>
2023-12-10 15:53:55 -0500

    added all the learning requirments

--- Sketch.java
+++ Sketch.java
@@ -25,6 +25,10 @@ public class Sketch extends PApplet {
   boolean rightPressed = false;
   boolean leftPressed = false;
 
+  String Password = "";
+
+  boolean Entered = false;
+
   public void setup() {
 
     background(0);
@@ -35,19 +39,31 @@ public class Sketch extends PApplet {
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-	  
+
+    if (Password.equals("password")) {
+
+      Entered = true;
+
+    background(0);
     // moues pressed variable 
     if (mousePressed) {
-      background(0);
+      fill(180,180,180);
+      ellipse(mouseX,mouseY,20,20);
     } else {
-      background(255);
-    }
-
-    // moue location 
-    if (mouseX <= 200 && mouseY <= 200) {
+      if (mouseX <= 200 && mouseY <= 200) {
+      fill(255,0,255);
+      ellipse(mouseX,mouseY,intSizeX,intSizeY);
+    } else if (mouseX > 200 && mouseY > 200) {
+      fill(255,0,255);
+      ellipse(mouseX,mouseY,intSizeX,intSizeY);
+    } else if (mouseX > 200 && mouseY <= 200) {
+      fill(255,0,255);
+      ellipse(mouseX,mouseY,intSizeX,intSizeY);
+    } else if (mouseX <= 200 && mouseY > 200) {
       fill(255,0,255);
       ellipse(mouseX,mouseY,intSizeX,intSizeY);
     }
+  }
 
     // handling multiple keys 
     if (upPressed == true) {
@@ -63,14 +79,14 @@ public class Sketch extends PApplet {
     // key pressed variable 
     if (keyPressed) {
 
-        // key variable 
-        if (key == 'd' || key == 'D') {
-          fill(0,255,255);
-          rect(mouseX,mouseY,mouseX + 10, mouseY + 10);
+      // key variable 
+      if (key == 'd' || key == 'D') {
+        fill(0,255,255);
+        rect(mouseX,mouseY,mouseX + 10, mouseY + 10);
 
       } else if (key == 's' || key == 'S') {
-          fill(190,222,0);
-          rect(mouseX,mouseY,mouseX + 10, mouseY + 10);
+        fill(190,222,0);
+        rect(mouseX,mouseY,mouseX + 10, mouseY + 10);
 
       } else if  (key == 'a' || key == 'A') {
           fill(90,10,0);
@@ -79,21 +95,24 @@ public class Sketch extends PApplet {
       } else if  (key == 'w' || key == 'W') {
           fill(0,0,9);
           rect(mouseX,mouseY,mouseX + 10, mouseY + 10);
-
       }
 
-  }    
-
+      }    
+    } else  {
+      text(Password,250,250);
+    }
 }
-  
 
-  // mouse event 
-  public void mouseDragged() {
-    fill(180,180,180);
-    ellipse(mouseX,mouseY,20,20);
+public void keyTyped() {
+  if (Entered == false) {
+  Password += key;
+  } else if (Entered == true) {
+    
   }
+}
 
 public void mouseClicked() {
+  
   fill(200,200,200);
   ellipse(mouseX,mouseY,20,20);
 }
